de8857b0148696f986d8d668aef9d9bd8aeb0c2b
prashantcoding <soultaker399@gmail.com>
2024-08-11 12:27:40 +0530

    url correct

--- server.js
+++ server.js
@@ -14,11 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/api', groupRoutes);
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+app.use('/api', groupRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
